152ab38742150bfaff6e344afd604a16356ce47d
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-24 20:48:19 +0500

    home component is completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home'>
+      <div className="content">
         <h1>Welcome to Our Website</h1>
         <p>We create modern solution for your digital needs.</p>
         <button>Get Start</button>
+      </div>
     </div>
   )
 }
