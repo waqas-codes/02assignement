74fc5994a50e39b4c234b71e3e2d0dd6f7f059b9
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-24 21:03:34 +0500

    about component is completed

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <h1>About Us</h1>
+        <p>We are a passionate team of developers and designers, dedicated to building clean, modern, and functional websites that help businesses grow.</p>
+    </div>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import Home from './Home'
+import About from './About'
+import Services from './Services'
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <About />
+      <Services />
     </div>
   )
 }
